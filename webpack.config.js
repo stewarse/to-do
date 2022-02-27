a5d66215a923b02136b32035ec4e64f27b9ed213
Sean Stewart <stewarse@gmail.com>
2022-02-26 19:03:48 -0800

    Setting up webpack dev server and dev middleware

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,14 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath:'/',
     },
     module: {
         rules: [
